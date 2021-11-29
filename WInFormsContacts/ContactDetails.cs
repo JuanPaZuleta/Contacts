34c9ab451f17621a57f8c9ee56373f3ecb7f420e
HP <HP@DESKTOP-QQ00O5F>
2021-11-29 17:27:17 -0300

    Agregar archivos de proyecto.

---
+++ WInFormsContacts/ContactDetails.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WInFormsContacts
+{
+    public partial class ContactDetails : Form
+    {
+        private BusinessLogicLayers _businessLogicLayers;
+        private Contact _contact;
+        public ContactDetails()
+        {
+            InitializeComponent();
+            _businessLogicLayers = new BusinessLogicLayers();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveContact();
+            this.Close();
+            ((Main)this.Owner).Populatecontacts();
+        }
+        #region PRIVATE METHODS
+        private void SaveContact() {
+
+            Contact contact = new Contact();
+            contact.FirstName = txtFirstName.Text;
+            contact.LastName = txtLastName.Text;
+            contact.Phone = txtPhone.Text;
+            contact.Address = txtAddress.Text;
+
+            contact.Id = _contact != null ? _contact.Id : 0;
+
+            _businessLogicLayers.SaveContact(contact);
+        }
+        #endregion
+
+        public void LoadContact(Contact contact)
+        {
+            _contact = contact;
+            if (contact != null)
+            {
+                ClearFrom();
+
+                txtFirstName.Text = contact.FirstName;
+                txtLastName.Text = contact.LastName;
+                txtPhone.Text = contact.Phone;
+                txtAddress.Text = contact.Address;
+            }
+             
+        }
+        private void ClearFrom()
+        {
+            txtFirstName.Text = string.Empty;
+            txtLastName.Text = string.Empty;
+            txtPhone.Text = string.Empty;
+            txtAddress.Text = string.Empty;
+        }
+    }
+}
---
+++ WInFormsContacts/Main.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WInFormsContacts
+{
+    public partial class Main : Form
+    {
+
+        private BusinessLogicLayers _businessLogicLayers;
+
+        public Main()
+        {
+            InitializeComponent();
+            _businessLogicLayers = new BusinessLogicLayers();  
+        }
+
+
+        #region Events
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            OpenContactDetailsDialog();
+        }
+        #endregion
+
+        #region PRIVATE METHODS
+        private void OpenContactDetailsDialog()
+        {
+            ContactDetails contactDetails = new ContactDetails();
+            contactDetails.ShowDialog(this);
+        }
+        #endregion
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Populatecontacts();
+        }
+
+        public void Populatecontacts(string searchText = null) { 
+            List<Contact> contacts = _businessLogicLayers.GetContacts(searchText);
+            gridContacts.DataSource = contacts;
+        }
+
+        private void gridContacts_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewLinkCell cell = (DataGridViewLinkCell)gridContacts.Rows[e.RowIndex].Cells[e.ColumnIndex];
+
+            if (cell.Value.ToString() == "Edit")
+            {
+                ContactDetails contactDetails = new ContactDetails();
+                contactDetails.LoadContact(new Contact
+                {
+                    Id = int.Parse(gridContacts.Rows[e.RowIndex].Cells[0].Value.ToString()),
+                    FirstName = gridContacts.Rows[e.RowIndex].Cells[1].Value.ToString(),
+                    LastName = gridContacts.Rows[e.RowIndex].Cells[2].Value.ToString(),
+                    Phone = gridContacts.Rows[e.RowIndex].Cells[3].Value.ToString(),
+                    Address = gridContacts.Rows[e.RowIndex].Cells[4].Value.ToString(),
+                });
+                contactDetails.ShowDialog(this);
+            }
+            else if (cell.Value.ToString() == "Delete") {
+                DeleteContact(int.Parse(gridContacts.Rows[e.RowIndex].Cells[0].Value.ToString()));
+                Populatecontacts();
+            }
+        }
+        private void DeleteContact(int id) {
+            _businessLogicLayers.DeleteContact(id);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            Populatecontacts(txtSearch.Text);
+            txtSearch.Text = string.Empty;
+        }
+    }
+}
+    
